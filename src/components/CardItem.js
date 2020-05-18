5d9b6c5562c688a1417d54141ec39492405d72ab
alexawesomecode <alexawesomecode@gmail.com>
2020-05-18 16:56:50 +0000

    fixed eslint

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import appstyles from './styles/appstyles.module.css';
 
 class CardItem extends React.Component {
-
   constructor(props) {
-
     super(props);
-    this.state = { indicators: {summary: 'Analyzing...'} }
+    this.state = { indicators: { summary: 'Analyzing...' } };
   }
 
 
-
   async componentDidMount() {
-
+    /* eslint-disable react/destructuring-assignment */
     const { id } = this.props.match.params;
     /* eslint-enable react/destructuring-assignment */
     const symbol = `${id.toUpperCase().slice(0, 3)}/${id.toUpperCase().slice(-3)}`;
@@ -29,24 +26,18 @@ class CardItem extends React.Component {
     };
 
     const url = `https://fcsapi.com/api-v2/forex/indicators?symbol=${symbol}&period=1h&access_key=vNB8FiE2F9mlUEVMgI2QLEXJzVZPxRJmgIypksJaYZL1jARQ5U`;
-    const response = await fetch(url)
+    const response = await fetch(url);
     const resp = await response.json();
-
-    console.log(resp)
     const result = normalize(resp.response.indicators);
-    const pair = resp.info.symbol
+    const pair = resp.info.symbol;
 
-    this.setState({indicators: result, pair: pair })
-
+    this.setState({ indicators: result, pair });
   }
 
   render() {
-
-    const stats = this.state.indicators
-    const { pair } = this.state
+    const { indicators, pair } = this.state;
 
 
-    //const [stats, setStats] = useState({ summary: 'Analizyng...' });
     /* eslint-disable react/destructuring-assignment */
 
     return (
@@ -54,38 +45,38 @@ class CardItem extends React.Component {
 
         <h1>
           {' '}
-{pair}
+          {pair}
         </h1>
 
         <h2 style={{ backgroundColor: 'yellow' }}>
           {' '}
-          {stats.summary}
+          {indicators.summary}
         </h2>
         <div className={appstyles.card_indicators_details}>
           <span>
             <h3>RSI:</h3>
             {' '}
-            {stats.RSI14}
+            {indicators.RSI14}
           </span>
           <span>
             <h3>ATR:</h3>
             {' '}
-            {stats.ATR14}
+            {indicators.ATR14}
           </span>
           <span>
             <h3>MACD:</h3>
             {' '}
-            {stats.MACD12_26}
+            {indicators.MACD12_26}
           </span>
           <span>
             <h3>STOCH:</h3>
             {' '}
-            {stats.STOCH9_6}
+            {indicators.STOCH9_6}
           </span>
           <span>
             <h3>WILLIAMS:</h3>
             {' '}
-            {stats.williamsR}
+            {indicators.williamsR}
           </span>
         </div>
         <button type="button" className={appstyles.goback} onClick={() => this.props.history.push('/')}>GO BACK</button>
@@ -94,19 +85,27 @@ class CardItem extends React.Component {
 
     );
   }
-};
+}
 
+CardItem.defaultProps = {
+
+  match: {},
+  history: {},
+};
 CardItem.propTypes = {
 
- 
-  
+
   /* eslint-disable no-dupe-keys */
   match: PropTypes.objectOf(PropTypes.object).isRequired,
   match: PropTypes.shape({
     path: PropTypes.string.isRequired,
+    params: PropTypes.objectOf(PropTypes.any).isRequired,
   }),
-  history: PropTypes.objectOf(PropTypes.object).isRequired,
-  history: PropTypes.shape({ length: PropTypes.number.isRequired }),
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+  history: PropTypes.shape({
+    length: PropTypes.number.isRequired,
+    push: PropTypes.objectOf(PropTypes.any).isRequired,
+  }),
   /* eslint-enable no-dupe-keys */
 
 };
