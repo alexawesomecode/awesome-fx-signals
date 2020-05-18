8f24e8a464f9c6c5473498f559982302d7aaace4
alexawesomecode <alexawesomecode@gmail.com>
2020-05-18 17:55:41 +0000

    deplying files

---
+++ build/precache-manifest.d5529507aa61bff5479be9650b2dee44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e55cd145d0185d7854a7cbf60771ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78eba6cfaa60451137f",
+    "url": "/static/css/main.3a9c348e.chunk.css"
+  },
+  {
+    "revision": "13cf69532650ccc99243",
+    "url": "/static/js/2.8406533e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.8406533e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78eba6cfaa60451137f",
+    "url": "/static/js/main.e35a4e85.chunk.js"
+  },
+  {
+    "revision": "0b562b7dbfd3677c6f27",
+    "url": "/static/js/runtime-main.c5f51242.js"
+  }
+]);
